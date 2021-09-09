60b147891d7c6842d92180d7b6fd1930ac1d437f
FTB <fernandot371@gmail.com>
2021-09-09 14:11:59 -0500

    ex. using channel to synchronize gorutines

---
+++ 7-Channels/3-Channels_Sync_4.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var wg sync.WaitGroup
+	for i := 1; i <= 10; i++ { // This allows us to create MANY goroutines
+		//for i := 1; i <= 10000; i++ {
+		//for i := 1; i <= 100000; i++ { // there is not problem with 100000 gorutines
+		wg.Add(1)
+		go func(j int) {
+
+			var result = 0
+			goChan := make(chan int)
+			mainChan := make(chan string)
+			calculateSquare := func() {
+				time.Sleep(time.Second * 3) // Deliberate time delay
+				result = j * j
+				goChan <- result
+			}
+			reportResult := func() {
+				fmt.Println(j, "squared is", <-goChan)
+				// blocks until it can read something from goChan - printed
+				mainChan <- "You can quit now.  I'm done." // This is just for clarity.
+			}
+
+			go calculateSquare()
+			go reportResult()
+			<-mainChan // blocks until it can read something from mainChan - discarded
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+}
